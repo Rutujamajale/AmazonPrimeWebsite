edfe63f0f81c999c8b05054d34a785d07a57995f
Rutujamajale <rutujamajale24774@gmail.com>
2024-09-06 09:45:19 +0530

    Initial commit for Amazon Prime Video project

--- src/App.js
+++ src/App.js
@@ -4,20 +4,98 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <header className="header">
+      <a className="Logo" href="https://www.primevideo.com/offers/nonprimehomepage/ref=atv_nb_logo">Amazon Prime</a>
+      <a  className="SignIN" href="https://www.amazon.in/-/hi/ap/signin?openid.pape.max_auth_age=3600&openid.return_to=https%3A%2F%2Fwww.amazon.in%2Fspr%2Freturns%2Fgift&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=amzn_psr_desktop_in&openid.mode=checkid_setup&language=en_IN&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0">Sign In</a>
+     </header>
+     <div className="secondDiv">
+      <div className="colusalDiv">
+        <h1>Welcome to Prime Video</h1>
+        <p>Join Prime to watch latest movies, TV Shows and award winning Amazon-Originals</p>
+        <button className="button" ><a className="Logo" href="https://www.amazon.in/-/hi/ap/signin?openid.pape.max_auth_age=3600&openid.return_to=https%3A%2F%2Fwww.amazon.in%2Fspr%2Freturns%2Fgift&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=amzn_psr_desktop_in&openid.mode=checkid_setup&language=en_IN&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0">Login to join prime</a></button>
+
+      </div>
+
+     </div>
+
+     <div className="ThirdDiv">
+      <div className="colusalDiv2">
+        <h1>Great Entertainment</h1>
+        <p>With your prime membership, you have aceess to exclusive Amazon Originals,
+        Blockbuster BollyWood Movies and regional </p>
+        <button className="button" ><a className="Logo" href="https://www.amazon.in/-/hi/ap/signin?openid.pape.max_auth_age=3600&openid.return_to=https%3A%2F%2Fwww.amazon.in%2Fspr%2Freturns%2Fgift&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=amzn_psr_desktop_in&openid.mode=checkid_setup&language=en_IN&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0">Login to join prime</a></button>
+
+      </div>
+
+     </div>
+     <div className="ForthDiv">
+      <div className="colusalDiv">
+        <h1>One memberships, many benefits</h1>
+        <p>Your Prime membership also includes ad-free music along with unlimited free,
+        fast delivery on eligible items, exclusive accessto deals & more</p>
+        <button className="button" ><a className="Logo" href="https://www.amazon.in/-/hi/ap/signin?openid.pape.max_auth_age=3600&openid.return_to=https%3A%2F%2Fwww.amazon.in%2Fspr%2Freturns%2Fgift&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=amzn_psr_desktop_in&openid.mode=checkid_setup&language=en_IN&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0">Get Started</a></button>
+<h6>*Go to amazon.in/prime for more information</h6>
+      </div>
+
+     </div>
+     <div className="fiftDiv">
+      <div className="colusalDiv2">
+        <h1>Even better with Fire TV Stic</h1>
+        <p>The biggest movies and TV shows are always better on a big screen.Simply plug your Amazon Fire TV Stick and stream on any HDTV.Press the voice button on the remote and say the title you want to watch to find it in seconds.</p>
+        <button className="button" ><a className="Logo" href="https://www.amazon.in/-/hi/ap/signin?openid.pape.max_auth_age=3600&openid.return_to=https%3A%2F%2Fwww.amazon.in%2Fspr%2Freturns%2Fgift&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=amzn_psr_desktop_in&openid.mode=checkid_setup&language=en_IN&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0">Get Started</a></button>
+
+      </div>
+
+     </div>
+     <div className="multiContent">
+      <div className="multiContentBox">
+        <div className="first">
+          <img src="https://images-na.ssl-images-amazon.com/images/G/01/digital/video/Magellan_MLP/PV_Benefits_Devices.jpg" alt="digital-image" className="images"/>
+          <h2 className="MultiContectText" >Watch Anywhere</h2>
+          <p className="multiContentPara">Enjoy from the web or with the Prime Video app on your phone, tablet, or select Smart TVs — on up to 3 devices at once.</p>
+
+        </div>
+
+      </div>
+      <div className="multiContentBox">
+        <div className="first">
+          <img src="https://images-na.ssl-images-amazon.com/images/G/01/digital/video/Magellan_MLP/PV_Benefits_Download_IN.jpg" alt="Benifits-image" className="images"/>
+          <h2 className="MultiContectText" >Download and go</h2>
+          <p className="multiContentPara">Watch offline on the Prime Video app when you download titles to your iPhone, iPad, Tablet, or Android device.</p>
+
+        </div>
+        </div>
+        <div className="multiContentBox">
+        <div className="first">
+          <img src="https://images-na.ssl-images-amazon.com/images/G/01/digital/video/Magellan_MLP/300x300_Tile_1.png" alt="Benifits-image" className="images"/>
+          <h2 className="MultiContectText" >Data Saver</h2>
+          <p className="multiContentPara">Control data usage while downloading and watching videos on select phones or tablets.</p>
+
+        </div>
+        </div>
+
+     </div>
+     <div className="familyFriendly">
+      <div className="colusalDiv">
+        <h1>Family Friendly</h1>
+        <p>With easy to use Parental Controls and a dedicated kids page, enjoy secure, ad-free kids entertainment. Kids can enjoy popular TV shows like Peppa Pig, Powerpuff Girls, and Chhota Bheem.</p>
+        <button className="button" ><a className="Logo" href="https://www.amazon.in/-/hi/ap/signin?openid.pape.max_auth_age=3600&openid.return_to=https%3A%2F%2Fwww.amazon.in%2Fspr%2Freturns%2Fgift&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=amzn_psr_desktop_in&openid.mode=checkid_setup&language=en_IN&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0">Get Started</a></button>
+
+      </div>
+
+     </div>
+     <footer className="footer">
+      <div className="footerPrimeDiv"> Prime Videos</div>
+      <div className="footerAncholDiv">
+      <a href="https://www.amazon.in/gp/help/customer/display.html">Terms and Privacy Notice</a>
+      <a href="https://www.amazon.in/gp/help/customer/display.html">Send us feedback </a>
+      <a href="https://www.amazon.in/gp/help/customer/display.html">Help</a>
+      <span style={{color:"white"}}>© 1996-2021, Amazon.com, Inc. or its affiliates</span>
+      </div>
+
+
+     </footer>
+     
     </div>
   );
 }
